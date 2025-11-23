dc3bddc8f09980ab735561466a7c4208fed6ce3f
Sripadh <sripadhsujith2009@gmail.com>
2025-11-23 16:46:19 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+import yfinance as yf
+from flask import Flask,jsonify
+from flask_cors import CORS
+
+app=Flask('__name__')
+CORS(app)
+
+stocks=['RELIANCE.NS', 'SBIN.NS', 'ADANIENT.NS', 'ADANIPORTS.NS', 'AXISBANK.NS', 'BHARTIARTL.NS', 'BRITANNIA.NS', 'CIPLA.NS', 'DRREDDY.NS', 'HCLTECH.NS', 'HDFCBANK.NS', 'HINDUNILVR.NS', 'ICICIBANK.NS', 'INFY.NS', 'ITC.NS', 'LT.NS', 'ONGC.NS', 'POWERGRID.NS', 'TATAMOTORS.NS', 'TATASTEEL.NS', 'TITAN.NS', 'TCS.NS', 'WIPRO.NS', 'HEROMOTOCO.NS', 'INDUSINDBK.NS', 'APOLLOHOSP.NS']
+
+
+
+
+@app.route('/')
+def news():
+    result=[]
+
+
+
+    for j in stocks:
+        
+        ticker=yf.Ticker(j)
+        news=ticker.news
+       
+        
+        result.append({
+            'title':news[0]['content']['title'],
+            'summary':news[0]['content']['summary'],
+            'url':news[0]['content']['canonicalUrl']['url']
+        })
+    
+    return jsonify(result)
+
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
